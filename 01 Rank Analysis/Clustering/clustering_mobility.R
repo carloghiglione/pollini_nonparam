f1456e7b03047590949e73ac9762d285c94af426
JacopoGhirri <jacopo.ghirri@gmail.com>
2021-12-09 13:37:45 +0100

    clustering

---
+++ 01 Rank Analysis/Clustering/clustering_mobility.R
@@ -0,0 +1,32 @@
+#CLUSTERING
+
+#We are looking for a clustering structure among our countries based on the difference in mobility
+#Each country is represented in R2 by (total number of people going in, total number of people going out) during our time horizon
+#we first of all build the distance matrix (Euclidian distance)
+diss <- dist(Trips_ext$tot_in, Trips_ext$tot_out, method = 'euclidian')
+
+#Ward is the only clustering structure that does not just separate UK from the rest of Europe
+clus <- hclust(diss, method = 'ward.D2')
+x11()
+plot(clus)
+
+lab <- cutree(clus, 2)
+flowlab <- cbind(Country = Trips_ext$Country, label = lab)
+flowlab 
+Trips_ext$Country[which(lab == "2")]
+
+x11()
+plot(Trips_ext$tot_in, Trips_ext$tot_out, main = "Clustering structure", xlab = "Ingoing flow", ylab = "Outgoing flow")
+points(Trips_ext$tot_in[which(lab == "1")], Trips_ext$tot_out[which(lab == "1")], col = 'green')
+points(Trips_ext$tot_in[which(lab == "2")], Trips_ext$tot_out[which(lab == "2")], col = 'red')
+legend(fill = c('green', 'red'),legend = c("First cluster", "Second cluster"), "topleft")
+
+
+x11()
+plot(log(Trips_ext$tot_in), log(Trips_ext$tot_out), main = "Clustering structure", xlab = "Ingoing_flow_logscale", ylab = "Outgoing_flow_logscale")
+points(log(Trips_ext$tot_in[which(lab == "1")]), log(Trips_ext$tot_out[which(lab == "1")]), col = 'green')
+points(log(Trips_ext$tot_in[which(lab == "2")]), log(Trips_ext$tot_out[which(lab == "2")]), col = 'red')
+legend(fill = c('green', 'red'),legend = c("First cluster", "Second cluster"), "topleft")
+
+
+#So we can interpret the resulting clustering structure as "High mobility countries" vs "Low mobility countries"
